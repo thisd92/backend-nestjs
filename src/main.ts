4b5f9361838d0ac263e07272f237bb4095b372de
thisd92 <80495413+thisd92@users.noreply.github.com>
2024-06-10 18:03:08 -0300

    :books: install and first config swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder().setTitle('Docs').build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('swagger/docs', app, document)
+
   await app.listen(3000);
 }
 bootstrap();
